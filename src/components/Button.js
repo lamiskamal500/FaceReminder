b16a2f52dd0391e1dd7ad738f11d39ad9b348065
lamiskamal500 <lamiskamal665@gmail.com>
2022-12-21 17:03:46 +0200

    GP2032-3

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+
+const Button = (props)=>{
+    return(
+        <TouchableOpacity style={styles.button}>
+            <Text>
+                {props.buttonText}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+   
+    button:{
+        backgroundColor:'#1E232C',
+        color:'#FFFFFF',
+        borderRadius:8,
+        display:'flex',
+        alignItems:'center',
+        width:331,
+        height:56,
+        fontFamily:'Urbanist',
+        paddingVertical:20
+    }
+
+})
+export default Button;
--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,10 +1,10 @@
-import {View, TextInput } from 'react-native';
+import {View, TextInput,StyleSheet } from 'react-native';
 import React from 'react';
 const InputText=(props)=>{
     return(
         <View style = {styles.InputTextStyle}>
                <TextInput
-        placeholder={props.DefaultText}
+        placeholder="hello"
        
         />
         </View>
@@ -12,11 +12,13 @@ const InputText=(props)=>{
 };
 const styles = StyleSheet.create({
     InputTextStyle:{
-        backgroundColor: 'F7F8F9',
+        backgroundColor: '#F7F8F9',
         width:331,
         height:56,
-        borderRadius:10,
-        color:'8391A1'
+        borderRadius:8,
+        color:'#8391A1',
+        placeholderTextColor:'#8391A1',
+        borderColor:'#E8ECF4'
     },
     
 
