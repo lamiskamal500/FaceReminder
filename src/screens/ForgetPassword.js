83a631b836a0b849121577c3ba5d2bce53856395
mohamed zakaria <m.zzzzr55@gmail.com>
2022-12-22 16:57:07 +0200

    GP2023-8

--- src/screens/ForgetPassword.js
+++ src/screens/ForgetPassword.js
@@ -3,11 +3,53 @@ import { TouchableOpacity,Text,View,StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; 
 import InputText from '../components/InputText';
 import Button from '../components/Button';
+import BackIcon from '../components/BackIcon';
 const ForgetPassword = ()=>{
     return(
-        <View>
-            <Text>ForgetPassword</Text>
+        <View style={styles.ForgetPasswordScreen}>
+           <BackIcon/>
+           <View style={{width:'100%',}}>
+            <Text style={styles.ForgetText}>Forget Password?</Text>
+            </View>
+            <View style={{width:'100%',}}>
+                <Text style={styles.ForgetBigText}> Don't worry! It occurs. Please enter the email address linked with your account.</Text>
+            </View>
+            <InputText DefaultText='Enter your email'/>
+            <Button style={styles.SendMailButton} buttonText='Send Mail'/>
+
         </View>
-    )
-}
+    );
+};
+const styles = StyleSheet.create({
+    ForgetPasswordScreen:{
+        display:'flex',
+        alignItems:'center',
+        backgroundColor:'#FFFFFF',
+        height:'100%',
+    },
+    ForgetText:{
+        color:"#1E232C",
+        fontSize:30,
+        fontWeight:'bold',
+        fontFamily:'Urbanist',
+        marginTop:20,
+        marginBottom:10,
+        marginHorizontal:30,
+        alignItems:'flex-start',
+
+    },
+    ForgetBigText:{
+        color:"#8391A1",
+        fontWeight:"medium",
+        fontSize:16,
+        fontFamily:'Urbanist',
+        marginBottom:20,
+        marginHorizontal:22
+        
+    },
+    SendMailButton:{
+        marginTop:30,
+
+    }
+})
 export default ForgetPassword;
